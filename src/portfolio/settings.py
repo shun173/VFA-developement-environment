3ce0490521daf6d2884c84a5f9b28eef19a8dcfb
shun173 <juntailangdaojian4@gmal.com>
2020-07-25 16:43:53 +0900

    modify database settings

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': 'django_db',
         'USER': 'django_user',
         'PASSWORD': 'djangopass',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
